987a12f5d882805adffd5cc512f1aceff3e8b931
glezzz <alexglezk@gmail.com>
2021-02-08 16:59:33 +0100

    web app displaying correct data

--- src/main/java/com/coronatracker/coronavirustracker/controllers/HomeController.java
+++ src/main/java/com/coronatracker/coronavirustracker/controllers/HomeController.java
@@ -1,10 +1,13 @@
 package com.coronatracker.coronavirustracker.controllers;
 
+import com.coronatracker.coronavirustracker.models.LocationStats;
 import com.coronatracker.coronavirustracker.services.CoronaVirusDataService;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
+
+import java.util.List;
 
 @Controller
 public class HomeController {
@@ -14,7 +17,11 @@ public class HomeController {
 
     @GetMapping("/")
     public String home(Model model) {
-        model.addAttribute("locationStats", coronaVirusDataService.getAllStats());
+        List<LocationStats> allStats = coronaVirusDataService.getAllStats();
+        int totalReportedCases = allStats.stream().mapToInt(stat -> stat.getLatestTotalCases()).sum();
+        model.addAttribute("locationStats", allStats);
+        model.addAttribute("totalReportedCases", totalReportedCases);
+
         return "home";
     }
 }
